065cc978807ec6c7559197488264c9a2057e15f0
zhangyunhao <zhangyunhao116@gmail.com>
2021-07-25 03:58:03 +0100

    bitwise: fix amd64 errors

--- bitwise_other.go
+++ bitwise_other.go
@@ -1,12 +1,14 @@
-// +build !amd64,gccgo,appengine
+// +build !amd64
 
 package atomicx
+
+import "sync/atomic"
 
 func OrUint32(addr *uint32, val uint32) {
 	for {
 		old := atomic.LoadUint32(addr)
 		if old&val != val {
-			if atomic.CompareAndSwapUint32(addr, old, old|flags) {
+			if atomic.CompareAndSwapUint32(addr, old, old|val) {
 				return
 			}
 			continue
@@ -19,7 +21,7 @@ func OrUint64(addr *uint64, val uint64) {
 	for {
 		old := atomic.LoadUint64(addr)
 		if old&val != val {
-			if atomic.CompareAndSwapUint64(addr, old, old|flags) {
+			if atomic.CompareAndSwapUint64(addr, old, old|val) {
 				return
 			}
 			continue
@@ -28,11 +30,35 @@ func OrUint64(addr *uint64, val uint64) {
 	}
 }
 
+func XorUint32(addr *uint32, val uint32) {
+	if val == 0 {
+		return
+	}
+	for {
+		old := atomic.LoadUint32(addr)
+		if atomic.CompareAndSwapUint32(addr, old, old^val) {
+			return
+		}
+	}
+}
+
+func XorUint64(addr *uint64, val uint64) {
+	if val == 0 {
+		return
+	}
+	for {
+		old := atomic.LoadUint64(addr)
+		if atomic.CompareAndSwapUint64(addr, old, old^val) {
+			return
+		}
+	}
+}
+
 func AndUint32(addr *uint32, val uint32) {
 	for {
 		old := atomic.LoadUint32(addr)
 		if old != old&val {
-			if atomic.CompareAndSwapUint32(addr, old, old&flags) {
+			if atomic.CompareAndSwapUint32(addr, old, old&val) {
 				return
 			}
 			continue
@@ -45,7 +71,7 @@ func AndUint64(addr *uint64, val uint64) {
 	for {
 		old := atomic.LoadUint64(addr)
 		if old != old&val {
-			if atomic.CompareAndSwapUint64(addr, old, old&flags) {
+			if atomic.CompareAndSwapUint64(addr, old, old&val) {
 				return
 			}
 			continue
@@ -54,10 +80,10 @@ func AndUint64(addr *uint64, val uint64) {
 	}
 }
 
-func OrUint32IfNeeded(addr *uint32, val uint32) { return OrUint32(addr, val) }
+func OrUint32IfNeeded(addr *uint32, val uint32) { OrUint32(addr, val) }
 
-func OrUint64IfNeeded(addr *uint64, val uint64) { return OrUint64(addr, val) }
+func OrUint64IfNeeded(addr *uint64, val uint64) { OrUint64(addr, val) }
 
-func AndUint32IfNeeded(addr *uint32, val uint32) { return AndUint32(addr, val) }
+func AndUint32IfNeeded(addr *uint32, val uint32) { AndUint32(addr, val) }
 
-func AndUint64IfNeeded(addr *uint64, val uint64) { return AndUint64(addr, val) }
+func AndUint64IfNeeded(addr *uint64, val uint64) { AndUint64(addr, val) }
