534e5b1fd621efdb2164422d13d783945039f3da
zhangyunhao <zhangyunhao116@gmail.com>
2021-08-22 14:50:35 +0800

    new cas2 api

--- cas2.go
+++ cas2.go
@@ -6,6 +6,6 @@ type Uint128 [2]uint64
 
 // CompareAndSwapUint128(CMPXCHG16B) requires that the destination (memory) operand be 16-byte aligned.
 // See https://www.felixcloutier.com/x86/cmpxchg8b:cmpxchg16b.
-func CompareAndSwapUint128(addr *Uint128, old, new Uint128) (swapped bool)
+func CompareAndSwapUint128(addr *Uint128, old1, old2, new1, new2 uint64) (swapped bool)
 
 func LoadUint128(addr *Uint128) (val Uint128)
